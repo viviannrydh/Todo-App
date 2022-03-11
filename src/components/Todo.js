0573b96d462f087cb1979882de5278fbad8a673e
Viviann Rydh <rydh.vivian@gmail.com>
2022-03-11 12:08:28 +0100

    add more media query

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,8 +6,7 @@ import '../App.css'
 const Todo = ({ text, todo, todos, setTodos }) => {
   const handleDelete = () => { 
     setTodos(todos.filter(element => (
-     // element.id !== todo.id
-      console.log(element)
+      element.id !== todo.id
     )))
   }  
 
